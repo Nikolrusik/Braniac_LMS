7c701f08a4b88f879041f3c5ac0ff62263731de0
Ruslan <94397861+Nikolrusik@users.noreply.github.com>
2022-10-17 19:45:14 +0300

    Lesson 3 (#3)
    * Added project and app, installed static files
    
    * Added base.html file, changed template files
    
    * Moved navbar in 'includes/main_menu.html'
    
    * Added illumination on main menu items
    
    * Added pagination on page news
    
    * Added template filter

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,25 +5,35 @@ from django.views.generic import View, TemplateView
 # Create your views here.
 
 
-class Index(TemplateView):
+class IndexPageView(TemplateView):
     template_name = "mainapp/index.html"
 
 
-class News(TemplateView):
+class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        context['news_title'] = "Громкий новостной заголовок"
+        context[
+            'news_preview'
+        ] = "Предварительное описание, которое заинтересует каждого"
+        context['range'] = range(5)
+        return context
 
-class Courses(TemplateView):
+
+class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
 
 
-class Contacts(TemplateView):
+class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
 
 
-class Documentos(TemplateView):
+class DocumentosPageView(TemplateView):
     template_name = "mainapp/doc_site.html"
 
 
-class Login(TemplateView):
+class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
