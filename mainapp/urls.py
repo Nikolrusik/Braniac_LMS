d98aec92044f9c4321cab24679532e57d88d4475
Ruslan <94397861+Nikolrusik@users.noreply.github.com>
2022-10-17 19:54:55 +0300

    Lesson 4 (#5)
    * Added base.html file, changed template files
    
    * Moved navbar in 'includes/main_menu.html'
    
    * Added illumination on main menu items
    
    * Added pagination on page news
    
    * Added template filter
    
    * Added models Courses, News, Teathers, Lessons; Added NewsManager
    
    * Make migrations and migration, create fixtures and dumps models News, Courses
    
    * Added empty migration
    
    * Added missing models and templates, fixed files

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,11 +5,16 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.IndexPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("news/", views.NewsPageView.as_view()),
+    path("", views.IndexPageView.as_view(), name="main_page"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
     path("login/", views.LoginPageView.as_view()),
-    path("courses/", views.CoursesPageView.as_view()),
-    path("documentos/", views.DocumentosPageView.as_view()),
-
+    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
+    path("documentos/", views.DocumentosPageView.as_view(), name="documentos"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View, TemplateView
+from django.shortcuts import get_object_or_404
 
-from mainapp.models import News
+
+from mainapp.models import News, Courses
 # Create your views here.
 
 
@@ -17,16 +19,44 @@ class NewsPageView(TemplateView):
         # Get all previous data
         context = super().get_context_data(**kwargs)
 
-        context['news_title'] = "Громкий новостной заголовок"
-        context[
-            'news_preview'
-        ] = "Предварительное описание, которое заинтересует каждого"
+        context['news'] = News.objects.all()
         context['range'] = range(5)
+        return context
+
+
+class NewsPageDetailView(TemplateView):
+    template_name = "mainapp/news_detail.html"
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super().get_context_data(pk=pk, **kwargs)
+        context["news_object"] = get_object_or_404(News, pk=pk)
         return context
 
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(CoursesPageView, self).get_context_data(**kwargs)
+        context["objects"] = Courses.objects.all()[:7]
+        return context
+
+
+class CoursesDetailView(TemplateView):
+    template_name = "mainapp/courses_detail.html"
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super(CoursesDetailView, self).get_context_data(**kwargs)
+        context["course_object"] = get_object_or_404(
+            Courses, pk=pk
+        )
+        context["lessons"] = Lesson.objects.filter(
+            course=context["course_object"]
+        )
+        context["teachers"] = CourseTeachers.objects.filter(
+            course=context["course_object"]
+        )
+        return context
 
 
 class ContactsPageView(TemplateView):
