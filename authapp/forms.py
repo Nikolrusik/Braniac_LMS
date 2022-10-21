635b029300f7486f8b05cbbfdae420a76068b11d
unknown <nikolrusik@gmail.com>
2022-10-21 23:30:42 +0300

    User edit form

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,10 @@
+import os
 from dataclasses import fields
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.forms import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -14,6 +18,44 @@ class CustomUserCreationForm(UserCreationForm):
         'age',
         'avatar',
     ]
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'age',
+            'avatar',
+        ]
+        field_classes = {'username': UsernameField}
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    field_order = [
+        'username',
+        'password1',
+        'password2',
+        'email',
+        'first_name',
+        'last_name',
+        'age',
+        'avatar',
+    ]
+
+    def clean_avatar(self):
+        arg_as_str = "avatar"
+        if arg_as_str in self.changed_data and self.instance.avatar:
+            if os.path.exists(self.instance.avatar):
+                os.remove(self.instance.avatar)
+        return self.cleaned_data.get(arg_as_str)
+
+    def clean_age(self):
+        data = self.cleaned_data.get("age")
+        if data < 18 or data > 100:
+            raise ValidationError(_('Please, enter a valid age!'))
+        return data
 
     class Meta:
         model = get_user_model()
